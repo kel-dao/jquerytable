07c403e1b87679a315f8943be4ad01d2568d184f
kel-dao <dao.kellya@yahoo.com>
2017-05-04 10:07:18 -0700

    change jquery file and fix inputs

--- inputs.js
+++ inputs.js
@@ -3,20 +3,44 @@
 //    alert("You entered name()");
 //});
 
-$(document).ready(function(){
- $("Input").change(function() {
-     if ($(this).attr('name') == 'gender') {
-    var input_id = $(this).attr("name");
-    var input_name = input_id.split("-")[0];
-    var output_id = input_name + "-Output";
-    var value = $( this ).val();
-    $("#" + output_id).text( value );   
-     } else {
-    var input_id = $(this).attr("id");
-    var input_name = input_id.split("-")[0];
-    var output_id = input_name + "-Output";
-    var value = $( this ).val();
-    $("#" + output_id).text( value );   
-     }
-  });
-})
+//$(document).ready(function(){
+// $("input").change(function() {
+//     if ($(this).attr('name') == 'choice') {
+//    var input_id = $(this).attr("name");
+//    var input_name = input_id.split("-")[0];
+//    var output_id = input_name + "-output";
+//    var value = $( this ).val();
+//    $("#" + output_id).text( value );   
+//     } else {
+//    var input_id = $(this).attr("id");
+//    var input_name = input_id.split("-")[0];
+//    var output_id = input_name + "-output";
+//    var value = $( this ).val();
+//    $("#" + output_id).text( value );   
+//     }
+//  });
+//})
+//
+
+
+$('input, select, textarea').on('input change', valueEntered);
+
+function valueEntered() {
+	var element = $(this);
+	var row = element.closest("tr");
+
+	if(element.is(':radio')) {
+		row.find('input').val([element.val()]);
+		row.find('.answer').text(element.val());
+	}
+	else {
+		if(element.is(':checkbox')) {
+			row.find('input').prop('checked', element.prop('checked'));
+			row.find('.answer').text(element.prop('checked'));
+		}
+		else {
+			row.find('input, select, textarea').val(element.val());
+			row.find('.answer').text(element.val());
+		}
+	}
+}
